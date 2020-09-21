66e538204f6dc5be466455bdb2819ff0f04e6bb4
User <siphesihlemambikimba@gmail.com>
2020-09-21 16:31:15 +0200

    circumference exercise

--- day1.py
+++ day1.py
@@ -7,3 +7,12 @@ print("I was coded in " + class_name)
 class_students= "20"
 
 print("There are " + class_students + " students in " + class_name)
+
+student_name = input("What is your name: ")
+
+print("So your name is " + student_name)
+
+student_age=int(input("How old are you:"))
+print("So in 10 years you will be..")
+print(student_age + 10)
+
---
+++ day1_exercise1.py
@@ -0,0 +1,7 @@
+print("welcome to the circumference calculator")
+radius = int(input("enter radius"))
+circ= 2 * radius * + 3.14
+
+print("Your answer:" + str(circ))
+print(circ)
+
